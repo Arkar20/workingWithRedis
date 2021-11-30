e8904cfd69d35de22ce996f26a164415a33cab10
Ar Kar <arkar20011@gmail.com>
2021-11-30 12:48:32 +0630

    basic sorted set

--- routes/web.php
+++ routes/web.php
@@ -23,7 +23,34 @@ Route::get('/', function () {
 
 Route::get('/article/{id}',function($id){
 
-    $totalvisitors=Redis::incr("articles.{$id}.visists");
+    $totalvisitors=Redis::incr("articles.{$id}.visists");  //* also : works fine
 
     return view('article',compact('totalvisitors'));
 });
+
+
+
+
+Route::get('/videos',function(){
+
+    $videos=  Redis::zrevrange('trending_videos',0,2);
+
+    return $videos;
+});
+Route::get('/videos/{id}',function($id){
+
+      Redis::zincrby('trending_videos',1,$id);
+
+    //!   Redis::zremrangebyrank('trending_videos',0,-2); limiting the set 
+
+    return back();
+});
+
+
+
+//! flushall to remove all 
+
+//*List = Array
+//*Hash = Object
+//*Sets= Unique Array or List 
+//*Sorted Sets= Sorted according to score, which we defined date, number, etc
